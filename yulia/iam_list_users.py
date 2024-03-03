5297a0320685e4976b5deaa341dd504eabc4da94
Julia Rozova <julia.in.your.heart@gmail.com>
2024-03-03 21:30:49 +0200

    created the script to show users

--- yulia/iam_list_users.py
+++ yulia/iam_list_users.py
@@ -1,7 +1,5 @@
 import boto3
 def list_users():
-    # aws_secret_key_id = 'AKIA3FLD3H6BTCH4QUN4'
-    # aws_secret_access_key = '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'
 
     client = boto3.client('iam')
     response = client.list_users()
